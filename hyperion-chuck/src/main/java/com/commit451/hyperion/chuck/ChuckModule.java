697c041644690a023b522f70a64da2b4295dca6e
Boon Wai <boon@burpple.com>
2018-09-12 15:03:50 +0800

    Edited dependencies, updated layout and assets

--- hyperion-chuck/src/main/java/com/commit451/hyperion/chuck/ChuckModule.java
+++ hyperion-chuck/src/main/java/com/commit451/hyperion/chuck/ChuckModule.java
@@ -1,7 +1,6 @@
 package com.commit451.hyperion.chuck;
 
 import android.content.Context;
-import android.content.Intent;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import android.view.LayoutInflater;
@@ -20,9 +19,8 @@ class ChuckModule extends PluginModule {
         view.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                final Context context = v.getContext();
-                Intent intent = Chuck.getLaunchIntent(context);
-                context.startActivity(intent);
+                final Context context = getContext();
+                context.startActivity(Chuck.getLaunchIntent(context));
             }
         });
         return view;
